778e933764b96c52fa8a5f64546121537e93bba2
Tek N. Chaudhary <tekchau21@gmail.com>
2023-06-18 19:42:11 +0530

    profileFragment added code.

--- app/src/main/java/com/dineInOrder/application/Fragments/profileFragment.java
+++ app/src/main/java/com/dineInOrder/application/Fragments/profileFragment.java
@@ -1,62 +1,35 @@
 package com.dineInOrder.application.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.dineInOrder.application.Activities.LoginActivity;
 import com.dineInOrder.application.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link profileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
+
 public class profileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public profileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static profileFragment newInstance(String param1, String param2) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -64,11 +37,57 @@ public class profileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    DatabaseReference ref;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    private static final String TAG="User Profile";
+    TextView userName,userPhoneNumber,userEmail;
+    Button logoutBtn;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if (getActivity() != null) {
             getActivity().setTitle("Profile");
         }
+        if (auth.getCurrentUser() != null) {
+            logoutBtn=view.findViewById(R.id.logoutbtn);
+            userName=view.findViewById(R.id.userName);
+            userPhoneNumber=view.findViewById(R.id.userPhoneNumber);
+            userEmail=view.findViewById(R.id.userEmail);
+            ref= FirebaseDatabase.getInstance().getReference("Registered Users");
+            ref.child(Objects.requireNonNull(auth.getUid())).get().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    if(task.getResult().exists()){
+                        DataSnapshot ds= task.getResult();
+                        String fullName=String.valueOf(ds.child("fullName").getValue());
+                        String phoneNumber=String.valueOf(ds.child("phoneNumber").getValue());
+                        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser(); // Initialize firebaseUser
+                        if (firebaseUser != null) { // Check if firebaseUser is not null
+                            String Email = firebaseUser.getEmail();
+                            userName.setText(fullName);
+                            userPhoneNumber.setText(phoneNumber);
+                            userEmail.setText(Email);
+                        }
+                    }
+                }
+                try {
+                    Exception exception = task.getException();
+                    if (exception != null) {
+                        throw exception;
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception occurred: " + Log.getStackTraceString(e));
+                }
+            });
+        } else {
+            Log.e(TAG, "FirebaseAuth instance is null or user is not signed in");
+        }
+
+        logoutBtn.setOnClickListener(view1 -> {
+            Intent intent=new Intent(getContext(), LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
     }
 }
